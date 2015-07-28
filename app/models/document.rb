dcd6664331cd3110cf905091995f58754f830860
HaChan <hades.24791@gmail.com>
2015-07-28 17:03:45 +0700

    Search Attachment Files with elasticsearch-rails

---
+++ app/models/document.rb
@@ -0,0 +1,24 @@
+class Document < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  mount_uploader :document_attachment, DocumentAttachmentUploader
+
+  settings index: { number_of_shards: 1, number_of_replicas: 0 } do
+    mapping _source: {excludes: ['attachment']} do
+      indexes :id, type: 'integer'
+      indexes :title, type: 'string'
+      indexes :attachment, type: 'attachment', analyzer: 'english', index_options: 'offsets'
+    end
+  end
+
+  def attachment
+    return if document_attachment.nil? || document_attachment.file.nil?
+    path_to_attachment = document_attachment.file.file
+    Base64.encode64(open(path_to_attachment){ |file| file.read })
+  end
+
+  def as_indexed_json(options={})
+    as_json only: [:title, :attachment], methods: [:attachment]
+  end
+end
